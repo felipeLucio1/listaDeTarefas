7b952967da5b3dd4dae40bc030ff0068e6216740
felipe lucio <felipelucio2009@gmail.com>
2025-01-27 11:41:14 -0300

    feat: cria a lista de tarefas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.felipelucio.listadetarefas"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.felipelucio.listadetarefas"
