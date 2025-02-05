77a0216e9fb2d5939afb5b727f8c1f9e1d2d73e4
Felipe Lucio do Nascimento <felipelucio2009@gmail.com>
2025-02-05 19:10:38 -0300

    feat: adiciona navegação para tela principal

--- app/src/main/java/com/felipelucio/listadetarefas/view/navigation/NavigationController.kt
+++ app/src/main/java/com/felipelucio/listadetarefas/view/navigation/NavigationController.kt
@@ -20,6 +20,9 @@ fun NavigationController(
         modifier = modifier,
     ) {
         composable<TelaPrincipal> {
+            TelaPrincipal
+        }
+        composable<TelaPrincipal> {
             TelaPrincipalComponente(
                 modifier = modifier,
                 onNovaTarefaClick = {
